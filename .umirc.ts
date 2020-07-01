f831b6691a498a3f773b38edf6ccebee3756a485
Kanchan431 <66299227+Kanchan431@users.noreply.github.com>
2020-07-01 10:56:52 +0530

    commit

---
+++ .umirc.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  locale: { antd: true },
+
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/products', component: '@/pages/products' },
+
+  ],
+});
